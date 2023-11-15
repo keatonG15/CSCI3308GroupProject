365dad1a2357c82e9d537450cb93acd07653d0b8
Keaton Glassman <keatonglassman15@gmail.com>
2023-11-15 11:51:35 -0700

    Successul bug fix, home page works correctly, passing 5 test cases

--- project_code/src/test/server.spec.js
+++ project_code/src/test/server.spec.js
@@ -3,12 +3,14 @@ const server = require('../index.js'); //TO-DO Make sure the path to your index.
 // Importing libraries
 
 
+
 // Chai HTTP provides an interface for live integration testing of the API's.
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 chai.should();
 chai.use(chaiHttp);
 const {assert, expect} = chai;
+
 
 
 describe('Server!', () => {
@@ -24,11 +26,28 @@ describe('Server!', () => {
        done();
      });
  });
+ // Sample test case given to test / endpoint.
+ it('Returns the default welcome message', done => {
+   chai
+     .request(server)
+     .get('/welcome')
+     .end((err, res) => {
+       expect(res).to.have.status(200);
+       expect(res.body.status).to.equals('success');
+       assert.strictEqual(res.body.message, 'Welcome!');
+       done();
+     });
+ });
 });
+
 
 
  // ===========================================================================
  // TO-DO: Part A Login unit test case
+ // ===========================================================================
+ // TO-DO: Part A Login unit test case
+
+
 
 
 
@@ -48,20 +67,35 @@ describe('Testing Login APIs', () => {
        });
    });
 
-
-   it('Negative Login', done => {
+   // Sample test case given to test / endpoint.
+   it('Successfull Login', done => {
      chai
        .request(server)
        .post('/login')
-       .send({username: 'mattfregahgjnd', password: '123' })
+       .send({username: 'matt', password: '123' })
        .redirects(0)
        .end((err, res) => {
-         expect(res).to.have.status(200);
-         expect(res).to.not.redirect;
+         expect(res).to.have.status(302);
+         expect(res).to.redirectTo('/home');
          done();
        });
    });
- });
+
+
+    it('Negative Login', done => {
+      chai
+        .request(server)
+        .post('/login')
+        .send({username: 'mattfregahgjnd', password: '123' })
+        .redirects(0)
+        .end((err, res) => {
+          expect(res).to.have.status(200);
+          expect(res).to.not.redirect;
+          done();
+        });
+    });
+  
+});
 
 
 // describe('Testing home APIs', () => {
