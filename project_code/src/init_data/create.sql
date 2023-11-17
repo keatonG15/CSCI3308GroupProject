0c2252ced6466b1233a675782ddacf1325fae8a3
Alex Norby <alno1200@colorado.edu>
2023-11-17 13:10:14 -0700

    added to users table

--- project_code/src/init_data/create.sql
+++ project_code/src/init_data/create.sql
@@ -2,5 +2,16 @@ CREATE TABLE users(
 username varchar(50) PRIMARY KEY,--Primary key to user
 password char(60) NOT NULL, --password
 highscore int,
-currentscore int
+currentscore int,
+
+answersRight int,
+answersWrong int, 
+gamesPlayed int, 
+allTimeScore int
+
 );
+
+
+
+
+
