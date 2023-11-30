b20aa4978957406e1a3118d111596bf19056690d
Keaton Glassman <keatonglassman15@gmail.com>
2023-11-29 17:21:56 -0700

    Rewards updated

--- project_code/src/index.js
+++ project_code/src/index.js
@@ -88,7 +88,34 @@ app.get('/login', (req, res) => {
  res.render('pages/login.ejs');
 });
 
-
+app.post('/login',  (req,res)=>{
+  const Uname = req.body.username;
+  const getUser = `SELECT * FROM users WHERE username = '${Uname}';`;
+  db.one(getUser)
+   .then(async function (data){
+      
+  //    console.log('Username: ' + data.username + ' Password: ' + data.password);
+       console.log("User Inputted: " + req.body.username + " - " + req.body.password  + "\nTable Found: " + data.username + " - " + data.password);
+     
+       const match = await bcrypt.compare(req.body.password, data.password);
+  
+  
+     //  console.log("Match: " + match);
+       if(match){
+       req.session.user = data;
+       req.session.save();
+      // console.log("here");
+       res.redirect('/home'); //302
+       }else{
+         res.render("pages/login.ejs", {message: `Invalid username or password`, error: true});
+       }
+   })
+   .catch(function (err) {
+     res.render("pages/register.ejs", {message: `Username not found.`, error: true});
+      
+   });
+  });
+  
 app.get('/register', (req , res)=> {
 res.render('pages/register.ejs');
 });
@@ -98,7 +125,8 @@ app.post('/register', async (req, res) => {
  const username = req.body.username;
  const hash = await bcrypt.hash(req.body.password, 10);
 
- const query = `INSERT INTO users (username, password, highscore, currentScore, answers_right, answers_wrong, all_time_score, currency, profile_pic, lives, last_right) VALUES ('${username}', '${hash}', '0', '0', '0', '0', '0', '0', 'img/prof0.png', '3', '0');`;
+ const query = `INSERT INTO users (username, password, highscore, currentScore, answers_right, answers_wrong, all_time_score, currency, profile_pic, lives, last_right, points_2x, using_2x,  points_3x)
+  VALUES ('${username}', '${hash}', '0', '0', '0', '0', '0', '0', 'img/prof0.png', '3', '0', '0', '0', '0');`;
  // console.log('Username: ', username);
  // console.log('Password: ', hash);
  
@@ -114,43 +142,6 @@ app.post('/register', async (req, res) => {
 
 
 });
-
-
-
-
-app.post('/login',  (req,res)=>{
-const Uname = req.body.username;
-const getUser = `SELECT * FROM users WHERE username = '${Uname}';`;
-db.one(getUser)
- .then(async function (data){
-    
-//    console.log('Username: ' + data.username + ' Password: ' + data.password);
-     console.log("User Inputted: " + req.body.username + " - " + req.body.password  + "\nTable Found: " + data.username + " - " + data.password);
-   
-     const match = await bcrypt.compare(req.body.password, data.password);
-
-
-   //  console.log("Match: " + match);
-     if(match){
-     req.session.user = data;
-     req.session.save();
-    // console.log("here");
-     res.redirect('/home'); //302
-     }else{
-       res.render("pages/login.ejs", {message: `Invalid username or password`, error: true});
-     }
- })
- .catch(function (err) {
-   res.render("pages/register.ejs", {message: `Username not found.`, error: true});
-    
- });
-});
-
-
-
-
-
-
 
 
 const auth = (req, res, next) => {
@@ -177,7 +168,9 @@ console.log("Size", data.length);
    username: req.session.user.username,
  
    highscore: req.session.user.highscore,
-   profile_pic: req.session.user.profile_pic
+   profile_pic: req.session.user.profile_pic,
+   lives: req.session.user.lives,
+   points_2x: req.session.user.points_2x
 
 });
  })
@@ -189,10 +182,33 @@ console.log("Size", data.length);
 
 });
 
+app.post('/usedouble', (req,res) => {
+const query = `UPDATE users SET points_2x = $1, using_2x = $2 WHERE username = $3;`;
+db.any(query, [req.session.user.points_2x - 1, 1, req.session.user.username])
+.then(function (data) {
+// console.log("aaa", req.session.user.currAnswer);
+ req.session.user.points_2x -= 1,
+ req.session.user.using_2x = 1;
+ res.redirect('/play');
+})
+// if query execution fails
+// send error message
+.catch(function (err) {
+ res.render("pages/home.ejs", {message: "Error using points", error: true})
+});
+
+
+
+});
+
 
 app.get('/play', (req,res) => {
 
-
+if(req.session.user.using_2x == 1){
+  console.log("Using 2x");
+}else{
+  console.log("Not using 2x");
+}
 axios({
  url: `https://the-trivia-api.com/v2/questions`,
  method: 'GET',
@@ -233,9 +249,124 @@ axios({
 });
 
 
+
 app.get('/verifyAnswer', (req,res)=>{
 res.redirect('/verifyAnswer');
 });
+
+app.post('/verifyAnswer', (req, res) =>{
+
+
+  console.log("Score", req.session.user.currentscore);
+  if(req.session.user.using_2x == 1){
+  var newScore = req.session.user.currentscore + 20;
+  }else{
+    var newScore = req.session.user.currentscore + 10;
+  }
+  
+  //need help getting the asnwer the user selected
+  //need help getting the correct answer for comparison
+  console.log("Answer" , req.body);
+  //console.log("Correct Answer", results.data.correctAnswer);
+  if(req.body.answer.localeCompare(req.body.correctAnswer) == 0){
+  
+   console.log("Correct!!!")
+   var right = req.session.user.answers_right + 1;
+   var all_time = req.session.user.all_time_score + 10;
+
+
+  if(req.session.user.using_2x == 1){
+    var money = req.session.user.currency + 2;  
+  }else{
+      var money = req.session.user.currency + 1; 
+  }
+    
+   
+
+
+
+   const updateScore =
+       'update users set currentscore = $1, answers_right = $2, all_time_score = $3, currency = $4, last_right = $5 where username = $6 returning * ;';
+     // $1 and $2 will be replaced by req.body.name, req.body.username
+     db.any(updateScore, [newScore, right, all_time, money, '1', req.session.user.username])
+     .then(function (data) {
+      console.log("asdaskdjnakjask",data)
+      req.session.user.currentscore = newScore
+      req.session.user.answers_right = right
+      req.session.user.all_time_score = all_time
+      req.session.user.currency = money
+      req.session.user.last_right = '1';
+  
+       res.redirect('/play');
+     })
+     // if query execution fails
+     // send error message
+     .catch(function (err) {
+       return console.log(err);
+     });
+  
+  
+  }else{
+   console.log("Incorrect!!!")
+   var wrong = req.session.user.answers_wrong + 1;
+   var life = req.session.user.lives - 1;
+  
+   //check for highscore
+   if(life > 0){
+    const setlives = 
+    `update users set lives = $1, last_right = $2, answers_wrong = $3 where username = $4 returning * ;`;
+    db.any(setlives, [life , '0', wrong, req.session.user.username])
+     .then(function (data) {
+      req.session.user.lives = life;
+      req.session.user.last_right = '0';
+      req.session.user.answers_wrong = wrong;
+      res.redirect('/play');
+     })
+     .catch(function (err) {
+      return console.log(err);
+    });
+  
+   }else{
+   if(req.session.user.currentscore >= req.session.user.highscore){
+     const highscore =
+       'update users set highscore = $1, answers_wrong = $2 where username = $4 returning * ;';
+     // $1 and $2 will be replaced by req.body.name, req.body.username
+     db.any(highscore, [req.session.user.currentscore, wrong, '3',  req.session.user.username])
+     .then(function (data) {
+     // console.log("asdaskdjnakjask",data)
+      req.session.user.highscore = req.session.user.currentscore;
+      req.session.user.answers_wrong = wrong;
+      res.redirect('/gameOver');
+     })
+     // if query execution fails
+     // send error message
+     .catch(function (err) {
+       return console.log(err);
+     });
+     //no highscore
+   }else{
+    const update_wrong =
+    'update users set answers_wrong = $1 where username = $2 returning * ;';
+    db.any(update_wrong, [ wrong, req.session.user.username])
+     .then(function (data) {
+      req.session.user.answers_wrong = wrong;
+      res.redirect('/gameOver');
+     });
+  
+     
+   }
+  }
+   }
+  
+  
+  
+  
+  
+  
+  //res.redirect('/play');
+  
+  
+  });
 
 app.get('/change_pic', (req,res)=>{
 
@@ -256,104 +387,6 @@ db.any(query, [req.body.selection, req.session.user.username])
 });
 });
 
-app.post('/verifyAnswer', (req, res) =>{
-
-
-console.log("Score", req.session.user.currentscore);
-var newScore = req.session.user.currentscore + 10;
-
-//need help getting the asnwer the user selected
-//need help getting the correct answer for comparison
-console.log("Answer" , req.body);
-//console.log("Correct Answer", results.data.correctAnswer);
-if(req.body.answer.localeCompare(req.body.correctAnswer) == 0){
-
- console.log("Correct!!!")
- var right = req.session.user.answers_right + 1;
- var all_time = req.session.user.all_time_score + 10;
- var money = req.session.user.currency + 1; 
- const updateScore =
-     'update users set currentscore = $1, answers_right = $2, all_time_score = $3, currency = $4, last_right = $5 where username = $6 returning * ;';
-   // $1 and $2 will be replaced by req.body.name, req.body.username
-   db.any(updateScore, [newScore, right, all_time, money, '1', req.session.user.username])
-   .then(function (data) {
-    console.log("asdaskdjnakjask",data)
-    req.session.user.currentscore = newScore
-    req.session.user.answers_right = right
-    req.session.user.all_time_score = all_time
-    req.session.user.currency = money
-    req.session.user.last_right = '1';
-
-     res.redirect('/play');
-   })
-   // if query execution fails
-   // send error message
-   .catch(function (err) {
-     return console.log(err);
-   });
-
-
-}else{
- console.log("Incorrect!!!")
- var wrong = req.session.user.answers_wrong + 1;
- var life = req.session.user.lives - 1;
-
- //check for highscore
- if(life > 0){
-  const setlives = 
-  `update users set lives = $1, last_right = $2, answers_wrong = $3 where username = $4 returning * ;`;
-  db.any(setlives, [life , '0', wrong, req.session.user.username])
-   .then(function (data) {
-    req.session.user.lives = life;
-    req.session.user.last_right = '0';
-    req.session.user.answers_wrong = wrong;
-    res.redirect('/play');
-   })
-   .catch(function (err) {
-    return console.log(err);
-  });
-
- }else{
- if(req.session.user.currentscore >= req.session.user.highscore){
-   const highscore =
-     'update users set highscore = $1, answers_wrong = $2 where username = $4 returning * ;';
-   // $1 and $2 will be replaced by req.body.name, req.body.username
-   db.any(highscore, [req.session.user.currentscore, wrong, '3',  req.session.user.username])
-   .then(function (data) {
-   // console.log("asdaskdjnakjask",data)
-    req.session.user.highscore = req.session.user.currentscore;
-    req.session.user.answers_wrong = wrong;
-    res.redirect('/gameOver');
-   })
-   // if query execution fails
-   // send error message
-   .catch(function (err) {
-     return console.log(err);
-   });
-   //no highscore
- }else{
-  const update_wrong =
-  'update users set answers_wrong = $1 where username = $2 returning * ;';
-  db.any(update_wrong, [ wrong, req.session.user.username])
-   .then(function (data) {
-    req.session.user.answers_wrong = wrong;
-    res.redirect('/gameOver');
-   });
-
-   
- }
-}
- }
-
-
-
-
-
-
-//res.redirect('/play');
-
-
-});
 
 
 app.get('/gameOver', (req,res)=>{
@@ -364,13 +397,14 @@ app.get('/gameOver', (req,res)=>{
 
 
  const endGame =
-     'update users set currentscore = $1, lives = $2 where username = $3 returning * ;';
+     'update users set currentscore = $1, lives = $2, using_2x = $3 where username = $4 returning * ;';
    // $1 and $2 will be replaced by req.body.name, req.body.username
-   db.any(endGame, [reset, '3', req.session.user.username])
+   db.any(endGame, [reset, '3', '0', req.session.user.username])
    .then(function (data) {
    // console.log("aaa", req.session.user.currAnswer);
     req.session.user.currentscore = reset,
-    req.session.user.lives = '3';
+    req.session.user.lives = '3',
+    req.session.user.using_2x = '0';
     res.render('pages/gameOver', {currscore: tempScore, highscore: req.session.user.highscore, correctanswer: req.session.user.curranswer});
    })
    // if query execution fails
@@ -384,7 +418,166 @@ app.get('/gameOver', (req,res)=>{
 
 })
 
-
+app.get('/welcome', (req, res) => {
+  res.json({status: 'success', message: 'Welcome!'});
+ });
+ 
+ 
+ 
+ 
+ app.get('/profile', (req,res) =>{
+ // correct answers
+ res.render("pages/profile.ejs", 
+ {
+ username: req.session.user.username,
+ password:req.session.user.password,
+ highscore:req.session.user.highscore,
+ answers_right:req.session.user.answers_right,
+ answers_wrong:req.session.user.answers_wrong,
+ all_time_score:req.session.user.all_time_score,
+ currency:req.session.user.currency,
+ profile_pic: req.session.user.profile_pic
+ 
+ 
+ }
+ );
+ });
+ 
+ app.get('/rewards', (req,res) =>{
+   // correct answers
+   res.render("pages/rewards.ejs", 
+   {
+   username: req.session.user.username,
+   password:req.session.user.password,
+   highscore:req.session.user.highscore,
+   answers_right:req.session.user.answers_right,
+   answers_wrong:req.session.user.answers_wrong,
+   all_time_score:req.session.user.all_time_score,
+   currency:req.session.user.currency
+   
+   
+   }
+   );
+ 
+   });
+ 
+   app.post('/buyReward', (req, res) => {
+    var reward = req.body.reward;
+    var cost = req.body.cost;
+    const life = parseInt(req.session.user.lives) + 1;
+    const points = req.session.user.points_2x + 1;
+ 
+     if(req.session.user.currency - cost < 0){
+       console.log("Not enough credits");
+       res.render("pages/rewards.ejs", 
+         {
+         username: req.session.user.username,
+         password:req.session.user.password,
+         highscore:req.session.user.highscore,
+         answers_right:req.session.user.answers_right,
+         answers_wrong:req.session.user.answers_wrong,
+         all_time_score:req.session.user.all_time_score,
+         currency:req.session.user.currency,
+         message: "Not enough credits",
+         error: true
+         
+         
+         })
+     }else{
+   
+         //  console.log(req.session.user.username, " bought ", reward, " for ", cost, " credits!")
+         if(reward.localeCompare("extralife") == 0 ){
+         
+           console.log(life);
+           //console.log("life");
+           const sellitem = `UPDATE users SET currency = $1, lives = $2 WHERE username = $3;`;
+           db.any(sellitem, [req.session.user.currency - cost, life, req.session.user.username])
+           .then(function (data) {
+           // console.log("aaa", req.session.user.currAnswer);
+            req.session.user.currency -= cost,
+            req.session.user.lives = life;
+            res.render("pages/rewards.ejs", 
+             {
+             username: req.session.user.username,
+             password:req.session.user.password,
+             highscore:req.session.user.highscore,
+             answers_right:req.session.user.answers_right,
+             answers_wrong:req.session.user.answers_wrong,
+             all_time_score:req.session.user.all_time_score,
+             currency:req.session.user.currency,
+             message: "You bought an extra life!"
+             
+             
+             })
+           })
+           // if query execution fails
+           // send error message
+           .catch(function (err) {
+             res.render("pages/rewards.ejs", 
+             {
+             username: req.session.user.username,
+             password:req.session.user.password,
+             highscore:req.session.user.highscore,
+             answers_right:req.session.user.answers_right,
+             answers_wrong:req.session.user.answers_wrong,
+             all_time_score:req.session.user.all_time_score,
+             currency:req.session.user.currency,
+             message: "Error with transaction",
+             error: true
+             
+             
+             })
+           });
+        
+         }else{
+    
+           console.log(points);
+          // console.log("points");
+          const sellitem = `UPDATE users SET currency = $1, points_2x = $2 WHERE username = $3;`;
+          db.any(sellitem, [req.session.user.currency - cost, points, req.session.user.username])
+          .then(function (data) {
+          // console.log("aaa", req.session.user.currAnswer);
+           req.session.user.currency -= cost,
+           req.session.user.points_2x = points;
+           res.render("pages/rewards.ejs", 
+            {
+            username: req.session.user.username,
+            password:req.session.user.password,
+            highscore:req.session.user.highscore,
+            answers_right:req.session.user.answers_right,
+            answers_wrong:req.session.user.answers_wrong,
+            all_time_score:req.session.user.all_time_score,
+            currency:req.session.user.currency,
+            message: "You bought an double points!"
+            
+            
+            })
+          })
+          // if query execution fails
+          // send error message
+          .catch(function (err) {
+            res.render("pages/rewards.ejs", 
+            {
+            username: req.session.user.username,
+            password:req.session.user.password,
+            highscore:req.session.user.highscore,
+            answers_right:req.session.user.answers_right,
+            answers_wrong:req.session.user.answers_wrong,
+            all_time_score:req.session.user.all_time_score,
+            currency:req.session.user.currency,
+            message: "Error with transaction",
+            error: true
+            
+            
+            })
+          });
+       
+         }
+       
+     }
+    
+ });
+ 
 app.get('/logout', (req,res) =>{
  req.session.destroy();
  res.render('pages/login', {message: `Logged Out Successfully`});
@@ -396,79 +589,6 @@ app.get('/logout', (req,res) =>{
 
 
 
-app.get('/welcome', (req, res) => {
- res.json({status: 'success', message: 'Welcome!'});
-});
-
-
-
-
-app.get('/profile', (req,res) =>{
-// correct answers
-res.render("pages/profile.ejs", 
-{
-username: req.session.user.username,
-password:req.session.user.password,
-highscore:req.session.user.highscore,
-answers_right:req.session.user.answers_right,
-answers_wrong:req.session.user.answers_wrong,
-all_time_score:req.session.user.all_time_score,
-currency:req.session.user.currency,
-profile_pic: req.session.user.profile_pic
-
-
-}
-);
-});
-
-app.get('/rewards', (req,res) =>{
-  // correct answers
-  res.render("pages/rewards.ejs", 
-  {
-  username: req.session.user.username,
-  password:req.session.user.password,
-  highscore:req.session.user.highscore,
-  answers_right:req.session.user.answers_right,
-  answers_wrong:req.session.user.answers_wrong,
-  all_time_score:req.session.user.all_time_score,
-  currency:req.session.user.currency
-  
-  
-  }
-  );
-
-  });
-
-  app.post('/buyReward', (req, res) => {
-    const { username, reward, cost } = req.session.user;
-
-    // Check if the reward is a valid column name to prevent SQL Injection
-    const validRewards = ['points_2x', 'points_3x']; // Add other valid reward names here
-    if (!validRewards.includes(reward)) {
-        return res.status(400).send({ message: 'Invalid reward type' });
-    }
-
-    // SQL to update the reward count and subtract the cost
-    // Using parameterized queries for security
-    const sql = `
-        UPDATE users 
-        SET ${reward} = ${reward} + 1, currency = currency - ? 
-        WHERE username = ? AND currency >= ?
-    `;
-
-    db.query(sql, [cost, username, cost], (err, result) => {
-        if (err) {
-            console.error(err); // Log the error for debugging
-            res.status(500).send({ message: 'Error processing request' });
-        }
-        else if (result.affectedRows === 0) {
-            res.status(400).send({ message: 'Not enough currency or invalid user' });
-        }
-        else {
-            res.send({ message: 'Reward purchased successfully' });
-        }
-    });
-});
 
 
   
